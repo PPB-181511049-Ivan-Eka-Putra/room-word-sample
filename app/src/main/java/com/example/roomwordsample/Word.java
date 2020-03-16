ee23c8769c8549a74319a061c37a7d34b448c768
ivanetra <ivanekaputra24@gmail.com>
2020-03-16 11:01:01 +0700

    Update Word class with annotations

--- app/src/main/java/com/example/roomwordsample/Word.java
+++ app/src/main/java/com/example/roomwordsample/Word.java
@@ -1,4 +1,23 @@
 package com.example.roomwordsample;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
 public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo
+    private String mWord;
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
 }
