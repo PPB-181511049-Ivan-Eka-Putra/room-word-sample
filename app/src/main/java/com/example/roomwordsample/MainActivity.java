1699ef54b1d23129b83a4f54e7153d3107dec5d0
ivanetra <ivanekaputra24@gmail.com>
2020-03-16 13:52:20 +0700

    Create a member variable for the ViewModel

--- app/src/main/java/com/example/roomwordsample/MainActivity.java
+++ app/src/main/java/com/example/roomwordsample/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WordViewModel mWordViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
