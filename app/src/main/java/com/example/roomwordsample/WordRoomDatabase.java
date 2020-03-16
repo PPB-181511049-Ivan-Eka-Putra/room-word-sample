cbc57388152f609ee008f25c1a8a6f01ed0a3e33
ivanetra <ivanekaputra24@gmail.com>
2020-03-16 13:33:35 +0700

    Creating the callback within the WordRoomDatabase class

--- app/src/main/java/com/example/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordsample/WordRoomDatabase.java
@@ -8,6 +8,7 @@ import androidx.room.DatabaseConfiguration;
 import androidx.room.InvalidationTracker;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 import java.util.concurrent.Executor;
@@ -22,6 +23,31 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     private static volatile WordRoomDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
     static final ExecutorService databaseWriterExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+
+            super.onOpen(db);
+
+            // If you want to keep data through app restarts,
+            // comment out the following block
+
+            databaseWriterExecutor.execute(() -> {
+
+                // Populate the database in the background.
+                // If you want to start with more words, just add them.
+
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                Word word = new Word("Hello");
+                dao.insert(word);
+                word = new Word("World");
+                dao.insert(word);
+            });
+        }
+    };
 
     static WordRoomDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
